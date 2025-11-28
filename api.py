e582f092db754b2a8ceb7fea0d1a6f5d4ec3f1e8
Reza Roozbeh <daarya.co@gmail.com>
2025-11-28 11:42:38 +0330

    implement functions(post_login, post_logout, ...)

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
+import auth_service
 from auth_service import AuthService
 from todo_service import TodoService
+from models import User
 
 class ApiSimulator:
     auth_service: AuthService
@@ -7,6 +9,14 @@ class ApiSimulator:
     
     def __init__(self):
         pass
+
+    '''
+    def require_auth(func):
+        def wrapper(self, token = None, *args, **kwargs):
+            user = self.auth_service.get_user_by_token(token)
+            func(self, user, *args, **kwargs)
+        return wrapper
+    '''
 
     def post_register(self, username: str, password: str) -> dict:
         user = self.auth_service.register(username, password)
@@ -26,4 +36,47 @@ class ApiSimulator:
         if user is None:
             return {"error": "Unauthorized"}
         self.auth_service.logout(token)
-        return {"success":False}
+        return {"success": False}
+
+    def post_todo_create(self, token: str, title: str) -> dict:
+        user = self.auth_service.get_user_by_token(token)
+        if user is None:
+            return {"error": "Unauthorized"}
+        todo = self.todo_service.create_todo(user.id, title)
+        self.todo_service.db.save_todos()
+        return {todo.id:{
+            'id': todo.id,
+            'user_id': todo.user_id,
+            'title': todo.title,
+            'completed': todo.completed,
+            'created_at': todo.created_at,
+        }}
+
+    def get_todos(self, token: str) -> list[dict]:
+        user = self.auth_service.get_user_by_token(token)
+        if user is None:
+            return [{"error": "Unauthorized"}]
+        todos = self.todo_service.list_todos(user.id)
+        return [{todo.id:{
+            'id': todo.id,
+            'user_id': todo.user_id,
+            'title': todo.title,
+            'completed': todo.completed,
+            'created_at': todo.created_at
+        } for todo in todos}]
+
+    def post_todo_complete(self, token: str, todo_id: int) -> dict:
+        user = self.auth_service.get_user_by_token(token)
+        if user is None:
+            return {"error": "Unauthorized"}
+        self.todo_service.complete_todo(user.id, todo_id)
+        self.todo_service.db.save_todos()
+        return {"success": True}
+
+    def post_todo_delete(self, token: str, todo_id: int) -> dict:
+        user = self.auth_service.get_user_by_token(token)
+        if user is None:
+            return {"error": "Unauthorized"}
+        self.todo_service.delete_todo(user.id, todo_id)
+        self.todo_service.db.save_todos()
+        return {"success": True}

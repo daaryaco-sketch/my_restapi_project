c0ba79d41684ffc2ec5f4e6551378e8d16d33deb
Reza Roozbeh <daarya.co@gmail.com>
2025-11-28 13:51:50 +0330

    fix errors

--- auth_service.py
+++ auth_service.py
@@ -11,14 +11,16 @@ class AuthService:
     def __init__(self, db: JsonDatabase):
         self.db = db
         self.sessions = {}
-        self.db.load_users()
-        max_id = max([user.id for user in self.db.users])
-        _user_counter = max_id
+        if len(self.db.users) == 0:
+            max_id = 0
+        else:
+            max_id = max([user.id for user in self.db.users])
+        AuthService._user_counter = max_id
 
     def register(self, username: str, password: str) -> User:
         if username in [user.username for user in self.db.users]:
-            raise ValueError('This username is exists.')
-        self._user_counter += 1
+            raise ValueError('This username already exists.')
+        AuthService._user_counter += 1
         user = User(
             id=self._user_counter,
             username=username,
--- main.py
+++ main.py
@@ -12,29 +12,32 @@ if __name__ == "__main__":
         db.load_todos()
     except FileNotFoundError as e:
         print(e)
+    try:
+        authSrv = AuthService(db)
+        todoSrv = TodoService(db)
+        api = ApiSimulator(authSrv, todoSrv)
 
-    authSrv = AuthService(db)
-    todoSrv = TodoService(db)
-    api = ApiSimulator(authSrv, todoSrv)
+        user = api.post_register('reza02', '12345')
+        token = list(api.post_login('reza02', '12345').values())[0]
 
-    user = api.post_register('reza029', '12345')
-    token = list(api.post_login('reza029', '12345').values())[0]
+        api.post_todo_create(token, "Programing")
+        api.post_todo_create(token, "Designing")
+        api.post_todo_create(token, "Creating")
 
-    api.post_todo_create(token, "Programing")
-    api.post_todo_create(token, "Designing")
-    api.post_todo_create(token, "Creating")
+        list_dict_todos = api.get_todos(token)
+        for list_todo in list_dict_todos:
+            print({key:value for key,value in list_todo.items()})
 
-    list_dict_todos = api.get_todos(token)
-    for list_todo in list_dict_todos:
-        print({key:value for key,value in list_todo.items()})
+        api.post_todo_complete(token, 1)
+        api.post_todo_delete(token, 1)
 
-    api.post_todo_complete(token, 1)
-    api.post_todo_delete(token, 1)
+        list_dict_todos = api.get_todos(token)
+        for list_todo in list_dict_todos:
+            print({key: value for key, value in list_todo.items()})
 
-    list_dict_todos = api.get_todos(token)
-    for list_todo in list_dict_todos:
-        print({key: value for key, value in list_todo.items()})
+        print(api.post_logout(token))
 
-    print(api.post_logout(token))
+    except Exception as ex:
+        print(ex)
 
 

79cada0cf7fc0405621e3a86ef5c01a370fb4f95
Reza Roozbeh <daarya.co@gmail.com>
2025-11-28 11:14:12 +0330

    implement functions(post_login, post_logout, ...)

--- api.py
+++ api.py
@@ -1,4 +1,29 @@
 from auth_service import AuthService
+from todo_service import TodoService
 
 class ApiSimulator:
-    #auth_service = 
+    auth_service: AuthService
+    todo_service: TodoService
+    
+    def __init__(self):
+        pass
+
+    def post_register(self, username: str, password: str) -> dict:
+        user = self.auth_service.register(username, password)
+        return {user.id:{
+            'id': user.id,
+            'username': user.username,
+            'password': user.password_hash,
+            'created_at': user.created_at,
+        }}
+
+    def post_login(self, username: str, password: str) -> dict:
+        session_token = self.auth_service.login(username, password)
+        return {username:session_token}
+
+    def post_logout(self, token: str) -> dict:
+        user = self.auth_service.get_user_by_token(token)
+        if user is None:
+            return {"error": "Unauthorized"}
+        self.auth_service.logout(token)
+        return {"success":False}
